7b0bf0dd7e60e9a095a525aa264b57545c70da18
Rajab Karume <rkarume@gmail.com>
2023-03-02 13:58:43 +0300

    ✨added responsiveness to the side bar

--- src/components/sidebar/SideBar.js
+++ src/components/sidebar/SideBar.js
@@ -4,25 +4,36 @@ import "./SideBar.css"
 function SideBar(){
 
     return(
-        <div className="sidebarContainer">
-            <div className="h1-div">
+        <div>
+            <div className="hamburger-container">
                 <h1>DASHBOARD</h1>
+                <div className="hamburger">
+                        <span className="bar"></span>
+                        <span className="bar"></span>
+                        <span className="bar"></span>
+                </div>
             </div>
-            <div className="top-container">
-                <ul className="top-list">
-                    <li>Overview</li>
-                    <li>Properties</li>
-                    <li>Tenants</li>
-                    <li>Rent</li>
-                    <li>Announcement</li>
-                    <li>Bills</li>
-                </ul>
-            </div>
-            <div className="bottom-container">
-                <ul className="bottom-list">
-                    <li>Settings</li>
-                    <li>Log out</li>
-                </ul>
+            <div className="sidebarContainer">
+                <div className="h1-div">
+                    <h1>DASHBOARD</h1>
+                </div>
+                <div className="top-container">
+                    <ul className="top-list">
+                        <li>Overview</li>
+                        <li>Properties</li>
+                        <li>Tenants</li>
+                        <li>Rent</li>
+                        <li>Announcement</li>
+                        <li>Bills</li>
+                    </ul>
+                </div>
+                <div className="bottom-container">
+                    <ul className="bottom-list">
+                        <li>Settings</li>
+                        <li>Log out</li>
+                    </ul>
+                </div>
+            
             </div>
         </div>
     )
